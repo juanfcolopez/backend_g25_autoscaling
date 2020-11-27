fb0aaa30a072d839afc1bbdafdfc7c127789a5bf
Juanfcolopez <jflopez2@uc.cl>
2020-11-27 02:55:34 -0300

    Add githubactions conf

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -3,7 +3,9 @@ cp ~/local_env.yml ~/aws-codedeploy/config
 cd ~/aws-codedeploy
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml down
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml build
-sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run app rails db:migrate
-sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run app rails db:migrate RAILS_ENV=development
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run --rm web bundle install
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run --rm web yarn install
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run web rails db:migrate
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run web rails db:migrate RAILS_ENV=development
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml up -d  
 pm2 startOrReload ecosystem.config.js
