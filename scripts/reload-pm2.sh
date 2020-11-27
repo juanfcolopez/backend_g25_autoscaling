f2514c1b8e14df817c8ae687b3fa228c6c5b08f7
Juanfcolopez <jflopez2@uc.cl>
2020-11-27 03:57:24 -0300

    Add touch .env command

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp ~/local_env.yml ~/aws-codedeploy/config
 cd ~/aws-codedeploy
+touch .env
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml down
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml build
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run --rm web bundle install
